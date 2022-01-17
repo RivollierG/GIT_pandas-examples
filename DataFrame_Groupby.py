7a95d5e7d8bd1cf37a25e40502f90c371b25100f
Vincent <vincent.faure0@gmail.com>
2022-01-17 09:29:14 +0100

    Update by V1cent avec les conflits résolus (normalement)

--- DataFrame_Groupby.py
+++ DataFrame_Groupby.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
-<<<<<<< HEAD
 __author__ = 'V1cent'
-=======
 
-__author__ = 'Guillaume'
->>>>>>> 420ee9d2b121e1cf5e529886c3f30b5c582feadf
 
 import pandas as pd
 import numpy as np
@@ -12,7 +8,7 @@ import numpy as np
 # Load Data
 userHeader = ['user_id', 'gender', 'age', 'ocupation', 'zip']
 users = pd.read_csv('dataSet/users.txt', engine='python',
-<<<<<<< HEAD
+
                     sep='::', header=None, names=userHeader, encoding='ISO-8859-1')
 
 movieHeader = ['movie_id', 'title', 'genders']
@@ -22,29 +18,7 @@ movies = pd.read_csv('dataSet/movies.txt', engine='python',
 ratingHeader = ['user_id', 'movie_id', 'rating', 'timestamp']
 ratings = pd.read_csv('dataSet/ratings.txt', engine='python',
                       sep='::', header=None, names=ratingHeader, encoding='ISO-8859-1')
-=======
-<<<<<<< HEAD
-                    sep='::', header=None, names=userHeader,encoding = "ISO-8859-1")
 
-movieHeader = ['movie_id', 'title', 'genders']
-movies = pd.read_csv('dataSet/movies.txt', engine='python',
-                     sep='::', header=None, names=movieHeader,encoding =  "ISO-8859-1")
-
-ratingHeader = ['user_id', 'movie_id', 'rating', 'timestamp']
-ratings = pd.read_csv('dataSet/ratings.txt', engine='python',
-                      sep='::', header=None, names=ratingHeader,encoding = "ISO-8859-1")
-=======
-                    sep='::', header=None, names=userHeader, encoding='ISO-8859-1')
-
-movieHeader = ['movie_id', 'title', 'genders']
-movies = pd.read_csv('dataSet/movies.txt', engine='python',
-                     sep='::', header=None, names=movieHeader, encoding='ISO-8859-1')
-
-ratingHeader = ['user_id', 'movie_id', 'rating', 'timestamp']
-ratings = pd.read_csv('dataSet/ratings.txt', engine='python',
-                      sep='::', header=None, names=ratingHeader, encoding='ISO-8859-1')
->>>>>>> ef3cd2ca0201da89d5b97232e243e9a434610e93
->>>>>>> 420ee9d2b121e1cf5e529886c3f30b5c582feadf
 
 # Merge data
 mergeRatings = pd.merge(pd.merge(users, ratings), movies)
@@ -53,14 +27,10 @@ mergeRatings = pd.merge(pd.merge(users, ratings), movies)
 
 
 def cloneDF(df):
-<<<<<<< HEAD
-    return pd.DataFrame(df.values.copy(), df.index.copy(), df.columns.copy())
-=======
 
     a = pd.DataFrame(df.values.copy(), df.index.copy(), df.columns.copy())
     return a.apply(pd.to_numeric, errors = 'ignore')
 
->>>>>>> 420ee9d2b121e1cf5e529886c3f30b5c582feadf
 
 
 # Show Films with more votes. (groupby + sorted)
@@ -86,34 +56,11 @@ print('Films ratings info: \n%s' % dataRatings[:10])
 print('\n==================================================================\n')
 
 
-<<<<<<< HEAD
-# Show data ratings movies, applying a function (groupby + lambda function)
-myAvg = cloneDF(mergeRatings)
-myAvg = myAvg.groupby(['movie_id', 'title'])['rating'].agg(
-    SUM=np.sum, COUNT=np.size, AVG= np.mean, myAVG= lambda x: x.sum() / float(x.count()))
-print('My info ratings: \n%s' % myAvg[:10])
-print('\n==================================================================\n')
-
 
 # Sort data ratings by created field (groupby + lambda function + sorted)
 sortRatingsField_V1cent = cloneDF(mergeRatings)
 youpi = sortRatingsField.groupby(['movie_id', 'title'])['rating'].agg(
     COUNT= np.size, myAVG=lambda x: x.sum() / float(x.count())).sort_values('COUNT', ascending=False)
-=======
-
-# # Show data ratings movies, applying a function (groupby + lambda function)
-# myAvg = cloneDF(mergeRatings)
-# myAvg = myAvg.groupby(['movie_id', 'title'])['rating'].agg(
-#     {'SUM': np.sum, 'COUNT': np.size, 'AVG': np.mean, 'myAVG': lambda x: x.sum() / float(x.count())})
-# print('My info ratings: \n%s' % myAvg[:10])
-# print('\n==================================================================\n')
-
-
-# Sort data ratings by created field (groupby + lambda function + sorted)
-sortRatingsField = cloneDF(mergeRatings)
-sortRatingsField = sortRatingsField.groupby(['movie_id', 'title'])['rating'].agg(
-   COUNT= np.size, myAVG= lambda x: x.sum() / float(x.count())).sort_values('COUNT', ascending=False)
->>>>>>> 420ee9d2b121e1cf5e529886c3f30b5c582feadf
 print('My info sorted: \n%s' % sortRatingsField[:15])
 
 
